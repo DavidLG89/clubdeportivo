6ea873be15d77d53746d9a3278cfd5d4ccded60c
Alberto <albertocalec@gmail.com>
2026-09-12 18:38:49 -0300

    feat: implementacion de pantalla de login y menu principal, con estilos Figma

--- app/src/main/java/com/grupo3/clubdeportivo/menu_principal.kt
+++ app/src/main/java/com/grupo3/clubdeportivo/menu_principal.kt
@@ -1,29 +1,20 @@
 package com.grupo3.clubdeportivo
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class menu_principal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu_principal)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Buscar el botón por su ID y configurar el evento click
-        val btnIngresar = findViewById<Button>(R.id.btnIngresar)
-        btnIngresar.setOnClickListener {
-            val intent = Intent(this, menu_principal::class.java)
-            startActivity(intent)
         }
     }
 }
